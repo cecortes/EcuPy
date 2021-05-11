a7743926e17243abff92566776b963e73d760884
César López Cortés <cesarlopezcortes@hotmail.com>
2021-05-10 20:32:44 -0500

    Init final python code

---
+++ Code/Final/EcuGps1.py
@@ -0,0 +1,63 @@
+#Importamos las librerías necesarias
+import serial
+import pynmea2      #Se encarga de los mensajes NMEA
+import string
+import io
+import arrow        #Se encarga de manejar el datetimestamp
+
+#Globales
+BAUDRATE = 9600
+TIMEOUT = 1
+usbCom = 0
+
+########### Funciones #############################
+#Se encarga de inciar y configurar el UART
+def InitUart():
+    #Cada que se utiliza una variable global se debe indicar en la función que la llama
+    global BAUDRATE, TIMEOUT, usbCom
+
+    #Objetos
+    usbCom = serial.Serial('COM3')      #Para el caso de Linux '/dev/ttyUSB0' or '/dev/ttyAMA0'
+    usbCom.baudrate = BAUDRATE
+    usbCom.timeout = TIMEOUT
+
+    #Flush
+    usbCom.flushInput()
+
+######### MAIN ###################################
+
+#Iniciamos la comUART
+InitUart()
+
+#Wrapper para el buffer a texto
+usbWrp = io.TextIOWrapper(io.BufferedRWPair(usbCom, usbCom))
+
+while True:
+    #Control de errores
+    try:
+        #Feed hungry mounster
+        gpsNewMsg = usbWrp.readline()
+
+        #Validación del mensaje
+        if (gpsNewMsg[0:6] == "$GPRMC"):
+            #Proccess msg
+            gpsDat = pynmea2.parse(gpsNewMsg)
+            #Decode data and convert float to string only 6 decimals
+            fecha = str(gpsDat.datestamp)
+            hora = str(gpsDat.timestamp)
+            lat = str('%.8f' % gpsDat.latitude)
+            lon = str('%.8f' % gpsDat.longitude)
+            #Construction Arrow datetime stamp
+            fechaHora = arrow.get(fecha + " " + hora, 'YYYY-MM-DD HH:mm:ss')
+            #Convert to local time and format datetime stamp
+            fechaHoraLocal = fechaHora.to('America/Mexico_City')
+            fechaHoraLocal = fechaHoraLocal.format('DD-MM-YYYY,HH:mm:ss')          
+            #CVS
+            print(fechaHoraLocal + "," + lat + "," + lon)
+
+    except pynmea2.ParseError as e:
+        continue
+
+    except(KeyboardInterrupt):
+        print(">> Interrupción del teclado...")
+        break
