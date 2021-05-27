e5cba9104e14b80e1a32376b1d26c295b549672d
César López Cortés <cesarlopezcortes@hotmail.com>
2021-05-27 11:39:37 -0500

    CanBusRx.py added

--- Code/Can/CanBusRx.py
+++ Code/Can/CanBusRx.py
@@ -4,5 +4,6 @@ import can
 #Configuración del bus, canal can0 y velocidad 500KBaud
 bus = can.interface.Bus(bustype='socketcan', channel='can0', bitrate=500000)
 
+#Recibe los mensajes del buffer y los muestra en pantalla, de manera ciclíca el pprograma no sale hasta interrupt
 for msg in bus:
     print(msg)
